2ab67e84bfd1486a97cf0fe1852f6f79db6293d4
Leo <50770485+leoleoleo77@users.noreply.github.com>
2024-07-09 21:18:21 +0300

    Add files via upload

---
+++ Helper.py
@@ -0,0 +1,123 @@
+import random
+import sys
+
+ALPHABET = ("A","C","G","T")
+
+PATTERNS = (
+    "AATTGA",
+    "CGCTTAT",
+    "GGACTAT",
+    "TTATTCGTA"
+)
+
+VERBOSE = True
+
+def RandomInt(start, end):
+    return random.randint(start, end)
+
+def GetRandomSymbol():
+    return ALPHABET[RandomInt(0, 3)]
+
+def GenerateRandomStringOfSymbols(stringLength):
+    numberOfSymbols = RandomInt(1, stringLength)
+    stringOfSymbols = ""
+
+    for _ in range(numberOfSymbols):
+        stringOfSymbols += GetRandomSymbol()
+
+    return stringOfSymbols
+
+def AddStringOfSymbolsAtIndex(pattern, stringOfSymbols, index):
+    pattern = list(pattern)
+    pattern[index:index] = stringOfSymbols
+    return "".join(pattern)
+
+def AddStringOfSymbolsAtStart(pattern, stringOfSymbols):
+    printMessage(f"Initialising the string with {str(len(stringOfSymbols))} symbol(s)")
+    return AddStringOfSymbolsAtIndex(pattern, stringOfSymbols, 0)
+
+def AddStringOfSymbolsAtEnd(pattern, stringOfSymbols):
+    endOfString = len(pattern)
+
+    printMessage(f"Also adding the symbol(s) {str(stringOfSymbols)} at the end of the string")
+
+    return AddStringOfSymbolsAtIndex(pattern, stringOfSymbols, endOfString)
+
+# replaces a random number of symbols
+def ReplaceSymbols(pattern):
+    numberOfSymbolsToBeReplaced = RandomInt(0, 2)
+
+    if numberOfSymbolsToBeReplaced == 0:
+        printMessage("No symbols will be replaced/removed!")
+        return pattern
+    
+    printMessage(f"There will be {str(numberOfSymbolsToBeReplaced)} symbol(s) replaced/removed")
+
+    randomIndices = []
+    pattern = list(pattern)
+    patternLength = len(pattern)
+    for _ in range(numberOfSymbolsToBeReplaced):
+        SelectAndReplaceUniqueIndex(pattern, patternLength, randomIndices)
+
+    pattern = "".join(pattern).replace("_", "")
+    return pattern
+
+# selects a random index and replaces the symbol at that index using ReplaceSymbolAtIndex()
+def SelectAndReplaceUniqueIndex(pattern, patternLength, randomIndices):
+    randomIndex = RandomInt(0, patternLength - 1)
+
+    if randomIndex in randomIndices:
+        SelectAndReplaceUniqueIndex(pattern, patternLength, randomIndices)
+        return 
+    
+    ReplaceSymbolAtIndex(pattern, randomIndex)
+    randomIndices.append(randomIndex)
+
+# replaces the symbol at the index with a random symbol
+# if the symbol at the index is the same as the random symbol, it replaces it with "_" instead
+def ReplaceSymbolAtIndex(pattern, index):
+    ramdomSymbol = GetRandomSymbol()
+
+    if pattern[index] == ramdomSymbol: 
+        printMessage(f"Removing the symbol at index: {str(index)}({pattern[index]})")
+        #pattern.pop(index)
+        pattern[index] = "_"
+        return 
+    
+    printMessage(f"Replacing the symbol at index: {str(index)}({pattern[index]}) with {ramdomSymbol}")
+    pattern[index] = ramdomSymbol
+    return 
+
+def SelectUniqueIndex(mainList, secondaryList):
+    randomIndex = RandomInt(0, 49)
+
+    if randomIndex in secondaryList:
+        SelectUniqueIndex(mainList, secondaryList)
+        return 
+    
+    secondaryList.append(randomIndex)
+
+def ReplaceIndexWithSymbol(listOfIndices, listOfSymbols):
+    for i in range(len(listOfIndices)):
+        listOfIndices[i] = listOfSymbols[listOfIndices[i]]
+        
+    return listOfIndices
+
+def printDatasets(datasetA, datasetB):
+    sys.stdout = open('output/datasetA.txt', 'w')
+    for i in range(len(datasetA)):
+        print(datasetA[i])
+
+    sys.stdout = open('output/datasetB.txt', 'w')
+    for i in range(len(datasetB)):
+        print(datasetB[i]) 
+    
+    sys.stdout = sys.__stdout__
+
+def printMessage(message1, message2 = None):
+    if VERBOSE: 
+        if message2 is None:
+            print(message1)
+            return
+        print(message1)
+        print(message2)
---
+++ StringGenerator.py
@@ -0,0 +1,40 @@
+import Helper as h
+
+class StringGenerator:
+
+    def __init__(self):
+        self.string = ""
+        self.allStrings = []
+        self.datasetA = []
+        self.datasetB = []
+        self.Generate50Strings()
+        self.FillDatasets()
+
+    def Generate50Strings(self):
+        for _ in range(50):
+            h.printMessage("-STEP A:\n")
+            self.string = h.AddStringOfSymbolsAtStart(self.string, h.GenerateRandomStringOfSymbols(3))
+            h.printMessage(f"Result: {self.string}\n", "-STEP B:\n")
+
+            for pattern in h.PATTERNS:
+                h.printMessage(f"For the Pattern {pattern},")
+                pattern = h.ReplaceSymbols(pattern)
+                h.printMessage(f"Result: {pattern}\n")
+                self.string += pattern
+
+            h.printMessage("-STEP C:\n", "Adding all the patterns to the initial string")
+            self.string = h.AddStringOfSymbolsAtEnd(self.string, h.GenerateRandomStringOfSymbols(2))
+            self.allStrings.append(self.string)
+            h.printMessage(f"Result: {self.string}\n", "Doing this another 49 times...\n")
+            self.string = ""
+            h.VERBOSE = False
+    
+    def FillDatasets(self):
+        for _ in range(15):
+            h.SelectUniqueIndex(self.allStrings, self.datasetA)
+        for i in range(50):
+            if i not in self.datasetA:
+                self.datasetB.append(i)
+        self.datasetA = h.ReplaceIndexWithSymbol(self.datasetA, self.allStrings)
+        self.datasetB = h.ReplaceIndexWithSymbol(self.datasetB, self.allStrings)
+        h.printDatasets(self.datasetA, self.datasetB)
